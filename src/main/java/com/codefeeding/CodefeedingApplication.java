66e33526393f2a997847dbf758ab90e538f1a914
codefeeding99 <codefeeding@gmail.com>
2025-08-09 20:10:03 +0530

    pushing project code into github

---
+++ src/main/java/com/codefeeding/CodefeedingApplication.java
@@ -0,0 +1,13 @@
+package com.codefeeding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefeedingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefeedingApplication.class, args);
+	}
+
+}
